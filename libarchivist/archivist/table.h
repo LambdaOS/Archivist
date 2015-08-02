60ff2d3985cd0cf324895796f42617686a24920c
Quinn Evans <heddwch@lambdaos.org>
2015-08-02 17:05:43 -0600

    Transliterated FNV-1a hash algorithm and 64-bit constants.

--- libarchivist/archivist/table.h
+++ libarchivist/archivist/table.h
@@ -1,7 +1,10 @@
-#ifndef TABLE_H
-#define TABLE_H 1
+#ifndef ARCH_TABLE_H
+#define ARCH_TABLE_H 1
 #include <stdint.h>
 typedef uint64_t arch_hash_t;
+
+#define ARCH_FNV64_PRIME 1099511628211
+#define ARCH_FNV64_OFFSET_BASIS 14695981039346656037
 
 arch_hash_t arch_hash(const arch_record_t *record);
 #endif
--- libarchivist/table.c
+++ libarchivist/table.c
@@ -1,12 +1,15 @@
 #include "archivist/table.h"
 
-// djb2 by Dan Bernstein
-arch_hash_t hash_string(const lrecord_t *string)
+// FNV-1a
+arch_hash_t arch_hash(const lrecord_t *record)
 {
-  arch_hash_t hash = 5381;
+  arch_hash_t hash = ARCH_FNV64_OFFSET_BASIS;
   
-  for(arch_size_t i = 0; i < string->size; i++) {
-    hash = ((hash << 5) + hash) + string->data[i];
+  for(arch_size_t i = 0; i < (record->size * record->width); i++) {
+    hash ^= record->data[i];
+    hash *= ARCH_FNV64_PRIME;
   }
+
+  return hash;
 }
 

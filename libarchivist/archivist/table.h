cb9bd923028f9719bec8d487f8758e4ae859f419
Quinn Evans <heddwch@lambdaos.org>
2015-08-09 21:27:40 -0600

    Started writing ridiculously convoluted crap (aka archivistd)

--- libarchivist/archivist/table.h
+++ libarchivist/archivist/table.h
@@ -2,14 +2,33 @@
 #define ARCH_TABLE_H 1
 #include <sys/types.h>
 #include <stdint.h>
+#include "uuid.h"
+
+#define ARCH_FNV64_PRIME ((arch_hash_t)1099511628211U)
+#define ARCH_FNV64_OFFSET_BASIS ((arch_hash_t)14695981039346656037U)
+#define ARCH_HASH_MASK(size) (size - 1)
+
 typedef uint64_t arch_hash_t;
 
 typedef struct {
   arch_uuid_t key, value;
-} __attribute__((packed)) arch_table_member_t;
+} __attribute__((packed)) arch_slot_t;
 
-#define ARCH_FNV64_PRIME ((arch_hash_t)1099511628211U)
-#define ARCH_FNV64_OFFSET_BASIS ((arch_hash_t)14695981039346656037U)
+#define ARCH_CACHE_MIN 15
+typedef struct arch_cache_bucket {
+  arch_uuid_t uuid;
+  arch_record_t *record;
+  arch_cache_bucket *next;
+} arch_cache_bucket_t;
 
-arch_hash_t arch_hash(const arch_record_t *record);
+typedef struct {
+  size_t size, entries;
+  arch_cache_bucket_t *slots[];
+} arch_cache_t;
+
+arch_hash_t arch_hash_octets(void *datum, size_t count);
+arch_hash_t arch_hash_record(arch_record_t *record);
+arch_hash_t arch_hash_uuid(arch_uuid_t uuid);
+arch_record_t *arch_cache_get(arch_cache_t *cache, arch_uuid_t uuid);
+void arch_cache_insert(arch_cache_t *cache, arch_cache_bucket_t *bucket)
 #endif
--- libarchivist/table.c
+++ libarchivist/table.c
@@ -1,15 +1,66 @@
+#include <stdlib.h>
 #include "archivist/record.h"
 #include "archivist/table.h"
 
 // FNV-1a
-arch_hash_t arch_hash(const arch_record_t *record)
+arch_hash_t arch_hash_octets(void *datum, size_t count)
 {
+  uint8_t *octets = (uint8_t *)datum;
   arch_hash_t hash = ARCH_FNV64_OFFSET_BASIS;
   
-  for(arch_size_t i = 0; i < (record->size * record->width); i++) {
-    hash ^= record->data[i];
+  for(arch_size_t i = 0; i < count; i++) {
+    hash ^= octets[i];
     hash *= ARCH_FNV64_PRIME;
   }
 
   return hash;
+}
+
+arch_hash_t arch_hash_record(arch_record_t *record)
+{
+  return arch_hash_octets(record->data, (record->size * record->width));
+}
+
+arch_hash_t arch_hash_uuid(arch_uuid_t uuid)
+{
+  return arch_hash_octets(&uuid, sizeof(arch_uuid_t));
+}
+
+arch_record_t *arch_cache_get(arch_cache_t *cache, arch_uuid_t uuid)
+{
+  size_t hash_mask = ARCH_HASH_MASK(cache->size);
+
+  while (hash_mask > ARCH_CACHE_MIN) {
+    arch_cache_bucket_t **current_slot, *current_bucket;
+    current_slot = &cache->slots[arch_hash_uuid(uuid) & hash_mask];
+    current_bucket = *current_slot;
+    while(current_bucket) {
+      if(current_bucket->uuid == uuid) {
+	arch_record_t *record = current_bucket->record;
+	if(hash_mask != ARCH_HASH_MASK(cache->size)){
+	  current_bucket = *current_slot;
+	  *current_slot = NULL;
+	  while(current_bucket) {
+	    arch_cache_insert(cache,)
+}
+	}
+	return record;
+      } else {
+	current_bucket = current_bucket->next;
+      }
+    }
+  }
+  
+  return NULL;
+}
+
+void arch_cache_insert(arch_cache_t *cache, arch_cache_bucket_t *bucket)
+{
+  arch_cache_bucket_t **slot =
+    &cache->slots[arch_hash_uuid(bucket->uuid) & (cache->size - 1)];
+
+  bucket->next = *slot;
+  *slot = bucket;
+
+  return;
 }

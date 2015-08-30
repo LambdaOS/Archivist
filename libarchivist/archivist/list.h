3a714442c9ed1409842c5a9deba3ce7f412c0d04
Quinn Evans <heddwch@lambdaos.org>
2015-08-30 15:21:48 -0600

    Fix ARCH_CAR()'s marginal abstraction, define ARCH_CONS_BYTES, add tracking parameter to arch_cons_init()

--- libarchivist/archivist/list.h
+++ libarchivist/archivist/list.h
@@ -3,15 +3,17 @@
 #include "record.h"
 #include "uuid.h"
 
-#define ARCH_CAR(record) *(((arch_uuid_t *)arch_record_elt(record, 0)))
-#define ARCH_CDR(record) *(((arch_uuid_t *)arch_record_elt(record, 1)))
+#define ARCH_CONS_BYTES (sizeof(arch_record_t) + sizeof(arch_uuid_t) * 2)
+#define ARCH_CAR(record) (*(arch_uuid_t *)arch_record_elt(record, 0))
+#define ARCH_CDR(record) (*(arch_uuid_t *)arch_record_elt(record, 1))
 
 typedef struct {
   arch_record_t *head, *current;
   arch_record_getter_t getter;
 } arch_list_iterator_t;
 
-arch_record_t *arch_cons_init(arch_record_t *record, arch_uuid_t car, arch_uuid_t cdr, arch_uuid_t ancestor, arch_record_getter_t getter);
+arch_record_t *arch_cons_init(arch_record_t *record, arch_uuid_t car, arch_uuid_t cdr, bool tracking,
+			      arch_uuid_t ancestor, arch_record_getter_t getter);
 arch_list_iterator_t *arch_list_iterator(arch_record_t *head, arch_record_getter_t getter);
 arch_record_t *arch_list_iterate(arch_list_iterator_t *iterator);
 
--- libarchivist/list.c
+++ libarchivist/list.c
@@ -3,14 +3,18 @@
 #include "archivist/record.h"
 #include "archivist/uuid.h"
 
-arch_record_t *arch_cons_init(arch_record_t *record, arch_uuid_t car, arch_uuid_t cdr, arch_uuid_t ancestor, arch_record_getter_t getter)
+arch_record_t *arch_cons_init(arch_record_t *record, arch_uuid_t car, arch_uuid_t cdr, bool tracking,
+			      arch_uuid_t ancestor, arch_record_getter_t getter)
 {
   record = _arch_record_init(record, ancestor, ARCH_UUID_NIL, getter);
   record->type = ARCH_TYPE_CONS;
   record->width = sizeof(arch_uuid_t);
   record->size = 2;
+  if(tracking) record->flags = ARCH_FLAG_TRACKING;
+
   ARCH_CAR(record) = car;
   ARCH_CDR(record) = cdr;
+
   return record;
 }
 
@@ -31,13 +35,15 @@ arch_list_iterator_t *arch_list_iterator(arch_record_t *head, arch_record_getter
 arch_record_t *arch_list_iterate(arch_list_iterator_t *iterator)
 {
   arch_record_t *current = iterator->current;
-  if(ARCH_IS(CONS, current)) {
-    iterator->current = iterator->getter(ARCH_CDR(current));
-    return iterator->getter(ARCH_CAR(current));
-  }
-  if(ARCH_IS(NIL, current)) {
-    iterator->current = iterator->head;
-    return current;
+  if(current) {
+    if(ARCH_IS(CONS, current)) {
+      iterator->current = iterator->getter(ARCH_CDR(current));
+      return iterator->getter(ARCH_CAR(current));
+    }
+    if(ARCH_IS(NIL, current)) {
+      iterator->current = iterator->head;
+      return current;
+    }
   }
   return NULL;
 }
